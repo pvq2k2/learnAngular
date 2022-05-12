897f64c9015416e6ed001bc157ad41d2de4c512b
pvq2k2 <vanquyet2392002e@gmail.com>
2022-05-12 12:39:31 +0700

    update

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -5,11 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ProductComponent {
+  productList: { id: string; name: string, price: number, status: boolean; description: string}[] = [
+    { 
+      id: '1',
+      name: 'Product A',
+      price: 100,
+      status: true,
+      description: 'Product A description'
+    },
+    { 
+      id: '2',
+      name: 'Product B',
+      price: 200,
+      status: false,
+      description: 'Product B description'
+    },
+    { 
+      id: '3',
+      name: 'Product C',
+      price: 300,
+      status: true,
+      description: 'Product C description'
+    },
+    { 
+      id: '4',
+      name: 'Product D',
+      price: 400,
+      status: false,
+      description: 'Product D description'
+    }
+  ]
 
 }
