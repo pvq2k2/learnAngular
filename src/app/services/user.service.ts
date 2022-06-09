558270d9bbe09e2b5f9141b45b82de5ba8dd0295
pvq2k2 <vanquyet2392002e@gmail.com>
2022-06-09 13:10:46 +0700

    feat: User service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../models/User';
+
+const apiUrl = 'http://localhost:3000/users';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  constructor( private http: HttpClient) { }
+
+  getAllUsers(): Observable<IUser> {
+    return this.http.get<IUser>(apiUrl);
+  }
+  getUser(id: number | string): Observable<IUser> {
+    return this.http.get<IUser>(`${apiUrl}/${id}`);
+  }
+  addUser(user: IUser): Observable<IUser> {
+    return this.http.post<IUser>(apiUrl, user);
+  }
+  updateUser(user: IUser): Observable<IUser> {
+    return this.http.put<IUser>(`${apiUrl}/${user.id}`, user);
+  }
+  removeUser(id: number | string): Observable<IUser> {
+    return this.http.delete<IUser>(`${apiUrl}/${id}`);
+  }
+}
